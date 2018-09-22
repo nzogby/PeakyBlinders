a379d72316df23b8ef25e380e37881349909a5aa
Komas Aryal <33465202+komasary@users.noreply.github.com>
2018-09-22 17:18:24 -0400

    Update Gui.java
    Jlabel update

--- src/Project1/Gui.java
+++ src/Project1/Gui.java
@@ -1,6 +1,12 @@
 package Project1;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 public class Gui extends Frame implements ActionListener {
@@ -10,27 +16,45 @@ public class Gui extends Frame implements ActionListener {
 	private Label stocks;
 	private TextField searchbar;
 	private Button Nasdaq;
-
+	private Button Close;
+	private JFrame Nasdaq_Frame;
+	
 	private static final long serialVersionUID = 1L;
 	public Gui() {
-		stocks= new Label("Stocks");
+		stocks= new Label("Stocks",JLabel.LEFT);
 		add(stocks);
+		setSize(200,200);
+		setVisible(true);
 		
+//		Nasdaq_Frame= new JFrame();
+//		add(Nasdaq_Frame);
+//		setSize(200,200);
+//		setVisible(true);
 
-		searchbar = new TextField("",10);
+		searchbar = new TextField(10);
 		searchbar.setEditable(true);
 		add(searchbar);
+		setSize(15,10);
+		setVisible(true);
+		searchbar.addActionListener(this);
+
+		
 		
 		Nasdaq= new Button("Nasdaq");
 		add(Nasdaq);
 		setVisible(true);
-		
 		Nasdaq.addActionListener(this);
-		
 		setTitle("NasDaq Predictor");
-		setSize(1000,550);
+		setSize(550,550);
 		
 		setVisible(true);
+		
+		Close= new Button("Close");
+		add(Close);
+		setVisible(true);
+		setSize(550,550);
+		setVisible(true);
+		
 	}
 	 public static void main(String[] args) {
 	      new Gui();
@@ -41,6 +65,16 @@ public class Gui extends Frame implements ActionListener {
 		// TODO Auto-generated method stub	}
 			
 	System.out.println("Success");
-	
+	Desktop browser= Desktop.getDesktop();
+	try {
+		browser.browse(new URI("https://www.nasdaq.com/"));
+	}
+	catch(IOException err) {
+		
+	} catch (URISyntaxException err) {
+		
+		System.exit(0);
+			
+	}
 }
 }
