dae6df40c1f90e6934f56bc538ad8b26d7dfc358
Nic <Nic@DESKTOP-JDLOGL9>
2018-09-22 18:01:18 -0400

    Test Commit, also fix of non closing frame

--- src/Project1/Gui.java
+++ src/Project1/Gui.java
@@ -9,7 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 
-public class Gui extends Frame implements ActionListener {
+public class Gui extends JFrame implements ActionListener {
 	/**
 	 * 
 	 */
@@ -55,6 +55,7 @@ public class Gui extends Frame implements ActionListener {
 		setSize(550,550);
 		setVisible(true);
 		
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	 public static void main(String[] args) {
 	      new Gui();
