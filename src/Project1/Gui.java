98cc24d4d2965934da4199a217d4f31874f730af
Komas Aryal <33465202+komasary@users.noreply.github.com>
2018-09-23 19:13:00 -0400

    updates Menu

--- src/Project1/Gui.java
+++ src/Project1/Gui.java
@@ -1,4 +1,4 @@
-package Project1;
+ppackage Project1;
 
 	import java.awt.*;
 	import java.awt.event.*;
@@ -13,36 +13,33 @@ package Project1;
 
 
 	public class Gui extends JFrame implements ActionListener {
-		/** 
+		/**
 		 * 
 		 */
 		private Label stocks;
 		private TextField searchbar;
-		private Button Nasdaq;
-		private JFrame Frame;
 		
-		//Stuff I am adding for later addition to master
-		private String _searchie;
 		private JPanel _buttons;
 		private JPanel _bar;
 		
 		private static final long serialVersionUID = 1L;
 		public Gui() {
 			setLayout(new GridLayout(2,2));
+			
 			add(_buttons = new JPanel());
 			add(_bar = new JPanel());
 			stocks= new Label("Stocks",JLabel.LEFT);
 			_buttons.add(stocks);
+			
+			
 			JButton j;
 			_buttons.add(j = new JButton("Do Nothing"));
 			j.setVisible(true);
 			setSize(200,200);
 			setVisible(true);
 			
-//			Nasdaq_Frame= new JFrame();
-//			add(Nasdaq_Frame);
-//			setSize(200,200);
-//			setVisible(true);
+		
+
 
 			searchbar = new TextField("Type Here");
 			searchbar.setEditable(true);
@@ -51,15 +48,45 @@ package Project1;
 			searchbar.setVisible(true);
 			
 			searchbar.addActionListener(this);
-						
+			this.pack();
 			this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		}
-		 public static void main(String[] args) {
-		      new GUI_Testing();
-		      
-		   }
-		@Override
-		public void actionPerformed(ActionEvent evt) {
+
+			
+
+		
+			
+			
+			
+			
+			
+			JButton Menu;
+			
+			_buttons.add(Menu= new JButton("Menu To be"));
+			Menu.setVisible(true);
+			setSize(200,200);
+			setVisible(true);
+			Menu.addActionListener(new Action());
+			//searchbar.addTextListener(new Text());
+			
+		}static class Action implements ActionListener{
+				
+				public void actionPerformed (ActionEvent e) {
+					JFrame menuframe = new JFrame("Menu to be");
+					menuframe.setVisible(true);
+					menuframe.setSize(750,750);
+					JLabel menulabel= new JLabel("Menu that does nothing");
+					JPanel menupanel = new JPanel();
+					menuframe.add(menupanel);
+					menupanel.add(menulabel);
+				}
+				
+			}
+	
+			
+						
+			
+		     
+		   public void actionPerformed1(ActionEvent e) {
 			// TODO Auto-generated method stub	}
 				
 		System.out.println("Success");
@@ -74,5 +101,16 @@ package Project1;
 			System.exit(0);
 				
 		}
+		}
+		 public static void main(String[] args) {
+		      new Gui();
 	}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
+	
+	
