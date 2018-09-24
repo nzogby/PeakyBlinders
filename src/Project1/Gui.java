95171beea8dc315ec96cec6a1cf22df6161d48e0
unknown <nppasqua@buffalo.edu>
2018-09-23 20:24:32 -0400

    Searchbar clears prior to typing, removed test button

--- src/Project1/Gui.java
+++ src/Project1/Gui.java
@@ -1,4 +1,4 @@
-package Project1;
+ppackage Project1;
 
 	import java.awt.*;
 	import java.awt.event.*;
@@ -11,8 +11,6 @@ package Project1;
 	import javax.swing.JLabel;
 	import javax.swing.JPanel;
 
-import Project1.Gui.Action;
-
 
 	public class Gui extends JFrame implements ActionListener {
 		/**
@@ -20,43 +18,45 @@ import Project1.Gui.Action;
 		 */
 		private Label stocks;
 		private TextField searchbar;
-		private Button Nasdaq;
-		private JFrame SearchFrame;
 		
-		//Stuff I am adding for later addition to master
-		private String _searchie;
 		private JPanel _buttons;
 		private JPanel _bar;
-		private JPanel _sPanel;
 		
 		private static final long serialVersionUID = 1L;
 		public Gui() {
-			setLayout(new GridLayout(3,3));
+			setLayout(new GridLayout(2,2));
+			
 			add(_buttons = new JPanel());
 			add(_bar = new JPanel());
 			stocks= new Label("Stocks",JLabel.LEFT);
-			setSize(800,600);
+			_buttons.add(stocks);
+			
+			
+			JButton j;
+			_buttons.add(j = new JButton("Do Nothing"));
+			j.setVisible(true);
+			setSize(200,200);
 			setVisible(true);
 			
-//			Nasdaq_Frame= new JFrame();
-//			add(Nasdaq_Frame);
-//			setSize(200,200);
-//			setVisible(true);
+		
+
 
 			searchbar = new TextField("Type Here");
 			searchbar.setEditable(true);
 			_bar.add(searchbar);
-			searchbar.setSize(5,25);
+			searchbar.setSize(5,10);
 			searchbar.setVisible(true);
-			searchbar.addMouseListener(new MouseAdapter() {
-				  @Override
-				  public void mouseClicked(MouseEvent e) {
-				    searchbar.setText("");
-				  }
-				});
+			
 			searchbar.addActionListener(this);
-						
+			this.pack();
 			this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+			
+
+		
+			
+			
+			
 			
 			
 			JButton Menu;
@@ -80,27 +80,37 @@ import Project1.Gui.Action;
 					menupanel.add(menulabel);
 				}
 				
+			}
+	
 			
+						
+			
+		     
+		   public void actionPerformed1(ActionEvent e) {
+			// TODO Auto-generated method stub	}
+				
+		System.out.println("Success");
+		Desktop browser= Desktop.getDesktop();
+		try {
+			browser.browse(new URI("https://www.nasdaq.com/"));
+		}
+		catch(IOException err) {
+			
+		} catch (URISyntaxException err) {
+			
+			System.exit(0);
+				
+		}
 		}
 		 public static void main(String[] args) {
 		      new Gui();
-		      
-		   }
-		@Override 
-		public void actionPerformed(ActionEvent evt) {
-			// TODO Auto-generated method stub	}
-		SearchFrame = new JFrame("Searched Stock");
-		SearchFrame.setSize(800, 800); SearchFrame.setVisible(true);
-		_sPanel = new JPanel();
-		_sPanel.setVisible(true);
-		_searchie = searchbar.getText();
-		Label l; _sPanel.setLayout(new GridLayout(3,3));
-		_sPanel.add(l = new Label(_searchie,JLabel.CENTER));
-		_sPanel.add(l = new Label("Stock Info Coming Soon",JLabel.CENTER));
-		l.setVisible(true);
-		l.setFont(new Font ("Monospaced", Font.BOLD, 16));
-		SearchFrame.add(_sPanel);
-		System.out.println(_searchie);
-
 	}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
+	
+	
