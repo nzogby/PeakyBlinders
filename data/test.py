1ff54cf42e34db0b53ef460c76081ab7818d8abe
nate <njzogby@buffalo.edu>
2018-10-12 18:07:26 -0400

    added unit test for sector files

--- data/test.py
+++ data/test.py
@@ -5,7 +5,7 @@ import os.path
 
 class DataCollectionTest(unittest.TestCase):
 	
-	### CHECKS FOR BASIC INSTANTIATION
+### CHECKS FOR BASIC INSTANTIATION
 	def test_1(self):
 		test = collect.DataCollector()
 ### BEGIN menu_bar UNIT TESTS ###
@@ -17,22 +17,35 @@ class DataCollectionTest(unittest.TestCase):
             test = collect.DataCollector()
             test.obtain_data
 
+# checks to verify each stock scraped successfully
+
 	def test_3(self):
-		test = collect.DataCollector()
-                test.obtain_data()
-                with open("data_files/manifest.json", "r") as f:
-                    symbols = json.load(f)
-                test_result = True
-                for symbol in symbols['input_stocks']:
-                    if not os.path.isfile("data_files/storage/" + symbol + "_ts_data.json"):
-                        test_result = False
-                self.assertTrue(test_result)
+	    test = collect.DataCollector()
+            test.obtain_data()
+            with open("data_files/manifest.json", "r") as f:
+                symbols = json.load(f)
+            test_result = True
+            for symbol in symbols['input_stocks']:
+                if not os.path.isfile("data_files/storage/" + symbol + "_data.json"):
+                    test_result = False
+            self.assertTrue(test_result)
+
+# checks to see if index file is created during build
+
         def test_4(self):
             test = collect.DataCollector()
             test.obtain_data()
             test.build_index_file()
             self.assertTrue(os.path.isfile("data_files/index.json"))
 
+        def test_5(self):
+            test = collect.DataCollector()
+            test.obtain_data()
+            test.build_index_file()
+            test.build_sector_files()
+            test_result = True
+            for sector in test.sectors:
+                self.assertTrue(os.path.isfile("data_files/types/" + sector.replace(" ", "_") + ".json"))
 
 ### END menu_bar UNIT TESTS ###
 
