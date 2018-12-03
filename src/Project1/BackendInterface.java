42fa64a14d6656dd1ea23cd66d241969828d4030
Nic <nppasqua@buffalo.edu>
2018-12-03 00:27:03 -0500

    search algorithm properly added

--- src/Project1/BackendInterface.java
+++ src/Project1/BackendInterface.java
@@ -2,8 +2,11 @@ package Project1;
 
 import java.util.*;
 import java.io.*;
-import com.eclipsesource.json.*;
-//import com.google.gson;
+//import com.eclipsesource.json.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 /*
  * Use this Class to interface with the backend stock data
  *
@@ -27,7 +30,7 @@ public class BackendInterface {
 	private HashMap<String, File> grab_type_files() {
 		HashMap<String, File> type_files = new HashMap<String, File>();
 		for (File file : (new File(TYPE_FILES_PATH).listFiles())){
-			type_files.put(file.getName().replace("_", " ").replace(".json", ""), TYPE_FILES_PATH + file);
+			type_files.put(file.getName().replace("_", " ").replace(".json", ""),TYPE_FILES_PATH + file);
 		}
 		return type_files;
 	}
@@ -46,33 +49,50 @@ public class BackendInterface {
 		return type_list;
 	}
 
- public Arraylist<Arraylist<String>>  buttonReturn(String type){
-  JsonObject json = getJsonFile(_type_files.get(type));
-  Iterator comp = json.keys();
-  Arraylist<Arraylist<String>> sector = new Arraylist<Arraylist<String>>();
-  while(comp.hasNext()){
-   Object key = comp.next();
-   int value = json.getJsonObject(key).get(price);
-   String name = json.getJsonObject(key).get(company).get(companyName);
-   Arraylist<String> companyInfo = new Arraylist<String>();
-   companyInfo.add(name); companyInfo.add(""+value);
-   sector.add(companyInfo);
-  }
-  return sector;
+ public ArrayList<ArrayList<String>>  buttonReturn(String type){
+//  	json_info_grabber jig = new json_info_grabber();
+//		ArrayList<ArrayList<String>> list = jig.info_grabber(type);
+	ArrayList<ArrayList<String>> list = new ArrayList<ArrayList<String>>();
+	JSONParser parser = new JSONParser();
+	File fil = new File(TYPE_FILES_PATH);
+	File[] files = fil.listFiles();
+	if(files !=null){
+			String toParse = TYPE_FILES_PATH+type+".json";
+			FileReader fr = new FileReader(toParse);
+			JSONArray a = (JSONArray) parser.parse(fr);
+			for(Object o :a){
+				JSONObject company = (JSONObject) o;
+				ArrayList<String> comp = new ArrayList<String>();
+				for(Object key : company.keySet()){
+					comp.add((String) company.get(key));
+				}
+				list.add(comp);
+			}
+	}
+  return list;
  }
 
-	public static JsonObject getJsonFile(File type){
-		String jsonString = "";
-		JsonObject JObj = Json.parse(jsonString).asObject();
-		return JObj;
+	public ArrayList<String> searching(String company){
+		ArrayList<String> list = new ArrayList<String>();
+		JSONParser parser = new JSONParser();
+		FileReader fil = new FileReader(_index_file+".json");
+		JSONArray a = (JSONArray) parser.parse(fil);		
+		for(Object o : a){
+			JSONObject jo = (JSONObject) o;
+			if(jo.get("companyName").equals(company) || jo.get("symbol").equals(company)){
+				ArrayList<String> comp = new ArrayList<String>();
+				for(Object key: jo.keySet()){
+					comp.add((String) jo.get(key));
+				}
+				return comp;
+			}
+			else{
+				ArrayList<String> fail = new ArrayList<String>();
+				fail.add("fail");fail.add(company);
+				return fail;
+			}
 		}
-
-	/*public ArrayList<String> grab_stocknames_from_type(String type){
-		File type_file = _type_files.get(type);
-		ArrayList<String> stock_names = new ArrayList<String>();
-
 	}
-*/
 	
 }
 
